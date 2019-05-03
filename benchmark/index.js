7fe094fdaa3c0f0cae6530a43763609c4ca3f71c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-03 14:57:53 +0800

    Create index.js

---
+++ benchmark/index.js
@@ -0,0 +1,39 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+const tests = [
+  'abs',
+  'acos',
+  'acosh',
+  'add',
+  'arg',
+  'asin',
+  'asinh',
+  'atan',
+  'atanh',
+  'ceil',
+  'conjugate',
+  'cos',
+  'cosh',
+  'div',
+  'exp',
+  'floor',
+  'ln',
+  'log10',
+  'mul',
+  'negate',
+  'pow',
+  'reciprocal',
+  'sign',
+  'sin',
+  'sinh',
+  'sqrt',
+  'sub',
+  'tan',
+  'tanh',
+];
+
+for (let i = 0; i < tests.length; i += 1) {
+  console.log('');
+  console.log(`${tests[i]}:`);
+  require(`./${tests[i]}`);
+}
