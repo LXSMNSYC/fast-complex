0c89421142389a097e2c5dea809ceae00512658f
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-03 14:57:51 +0800

    Create floor.js

---
+++ benchmark/floor.js
@@ -0,0 +1,26 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+/* eslint-disable func-names */
+const Benchmark = require('benchmark');
+
+const Complex = require('complex.js');
+const { floor } = require('../index');
+
+const suite = new Benchmark.Suite();
+
+const i = Math.random();
+
+suite.add('complex.js#floor', () => {
+  new Complex([i, i]).floor();
+});
+suite.add('fast-complex#floor', () => {
+  floor([i, i]);
+});
+suite.on('cycle', (event) => {
+  console.log(String(event.target));
+});
+suite.on('complete', function () {
+  console.log(`Fastest is ${this.filter('fastest').map('name')}`);
+});
+suite.run({ async: false });
