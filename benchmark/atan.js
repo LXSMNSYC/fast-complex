a01ba5d6f0f5dad1a5d0c7cf9d205010db8d2fcd
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-03 14:57:34 +0800

    Create atan.js

---
+++ benchmark/atan.js
@@ -0,0 +1,26 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+/* eslint-disable func-names */
+const Benchmark = require('benchmark');
+
+const Complex = require('complex.js');
+const { atan } = require('../index');
+
+const suite = new Benchmark.Suite();
+
+const i = Math.random();
+
+suite.add('complex.js#atan', () => {
+  new Complex([i, i]).atan();
+});
+suite.add('fast-complex#atan', () => {
+  atan([i, i]);
+});
+suite.on('cycle', (event) => {
+  console.log(String(event.target));
+});
+suite.on('complete', function () {
+  console.log(`Fastest is ${this.filter('fastest').map('name')}`);
+});
+suite.run({ async: false });
