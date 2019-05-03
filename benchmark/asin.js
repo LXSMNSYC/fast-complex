589c631a3f44fc5625f94a4522f9721b6e06dd4b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-03 14:57:31 +0800

    Create asin.js

---
+++ benchmark/asin.js
@@ -0,0 +1,27 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+/* eslint-disable func-names */
+const Benchmark = require('benchmark');
+
+const Complex = require('complex.js');
+const { asin } = require('../index');
+
+const suite = new Benchmark.Suite();
+
+const i = Math.random();
+
+
+suite.add('complex.js#asin', () => {
+  new Complex([i, i]).asin();
+});
+suite.add('fast-complex#asin', () => {
+  asin([i, i]);
+});
+suite.on('cycle', (event) => {
+  console.log(String(event.target));
+});
+suite.on('complete', function () {
+  console.log(`Fastest is ${this.filter('fastest').map('name')}`);
+});
+suite.run({ async: false });
